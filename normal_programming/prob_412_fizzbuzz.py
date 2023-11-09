6b05fc11ec460fd7cdacec0bdaffb2d89bd7337e
Karan Tandon <karantandon1991@gmail.com>
2023-11-09 11:05:43 +0530

    FizzBuzz problem

---
+++ normal_programming/prob_412_fizzbuzz.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+        for i in range(n):
+            new_n = i+1
+            if new_n%3==0 and new_n%5==0:
+                ans.append("FizzBuzz")
+            elif new_n%3==0:
+                ans.append("Fizz")
+            elif new_n%5==0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(new_n))
+        return ans
